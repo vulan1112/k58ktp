6a4f10242d0b1908509ca95b1294841b26107c24
vulan1112 <vulan2k4bg@gmail.com>
2025-03-30 17:20:14 +0700

    Add files via upload

---
+++ SQL_BAI3/SQLtruyvan.sql
@@ -0,0 +1,14 @@
+﻿SELECT
+    sv.MaSV,
+    sv.HoTen,
+    lhp.TenLopHP,
+    mh.TenMon,
+    lhp.HK,
+    SUM(d.Diem) AS TongDiem, -- Tổng điểm
+    (SUM(d.Diem) * (1 - COALESCE(dkmh.PhanTramThi, 0) / 100.0)) AS DiemThanhPhan
+FROM SinhVien sv
+JOIN DKMH dkmh ON sv.MaSV = dkmh.MaSV
+JOIN LopHP lhp ON dkmh.MaLopHP = lhp.MaLopHP
+JOIN MonHoc mh ON lhp.MaMon = mh.MaMon
+JOIN Diem d ON dkmh.id_dk = d.id_dk
+GROUP BY sv.MaSV, sv.HoTen, lhp.TenLopHP, mh.TenMon, lhp.HK, dkmh.PhanTramThi;
